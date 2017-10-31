80d8e81dff3899ad8b804a29e2b188228de6075a
Olle Lauri Boström <ollebostr@gmail.com>
2017-10-31 22:34:59 +0100

    RED : Implementing tests for TransportationType-class

---
+++ test/TransporationType.test.js
@@ -0,0 +1,44 @@
+import chai, { expect } from 'chai';
+import TransportationType from '../src/model/TransportationType';
+
+describe('{unit}: model/TransporationType', () => {
+  it('should have correct properties', () => {
+    const type = new TransportationType('METRO');
+    expect(type).to.have.all.keys('type', 'symbol');
+  });
+
+  it('should have correct symbol for type METRO', () => {
+    const type = new TransportationType('METRO');
+    expect(type).to.have.property('symbol', '🚇');
+  });
+
+  it('should have correct symbol for type BUS', () => {
+    const type = new TransportationType('BUS');
+    expect(type).to.have.property('symbol', '🚌');
+  });
+
+  it('should have correct symbol for type TRAIN', () => {
+    const type = new TransportationType('TRAIN');
+    expect(type).to.have.property('symbol', '🚆');
+  });
+
+  it('should have correct symbol for type TRAM', () => {
+    const type = new TransportationType('TRAM');
+    expect(type).to.have.property('symbol', '🚋');
+  });
+
+  it('should have correct symbol for type FERRY', () => {
+    const type = new TransportationType('FERRY');
+    expect(type).to.have.property('symbol', '⛴️');
+  });
+
+  it('should have correct symbol for type SHIP', () => {
+    const type = new TransportationType('SHIP');
+    expect(type).to.have.property('symbol', '🚢');
+  });
+
+  it('should use default symbol for unspecified types', () => {
+    const type = new TransportationType();
+    expect(type).to.have.property('symbol', '🔘');
+  });
+});
