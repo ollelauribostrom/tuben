26df08bd3423a4bc12e3b06ef8fac68bc0400a64
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-08 23:02:49 +0100

    GREEN: Adding requested properties to Journey contructor

--- src/model/Journey.js
+++ src/model/Journey.js
@@ -1,3 +1,11 @@
 export default class Journey {
 
+  constructor(journeyData = {}) {
+    this.from = journeyData.from;
+    this.to = journeyData.to;
+    this.departureTime = journeyData.departureTime;
+    this.arrivalTime = journeyData.arrivalTime;
+    this.departureDate = journeyData.departureDate;
+    this.arrivalDate = journeyData.arrivalDate;
+  }
 }
