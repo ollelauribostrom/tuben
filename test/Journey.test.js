f0ebb8b6650b29a2c5b7130ec1973c2ac37f204b
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-08 22:54:34 +0100

    RED: Implementing test to make sure Journey class has correct properties after instantiation

---
+++ test/Journey.test.js
@@ -0,0 +1,16 @@
+import chai, { expect } from 'chai';
+import Journey from '../src/model/Journey';
+
+describe('{unit}: model/Journey', () => {
+  it('should have correct properties after instantiation', () => {
+    const journey = new Journey();
+    expect(journey).to.have.all.keys(
+      'from',
+      'to',
+      'departureTime',
+      'arrivalTime',
+      'departureDate',
+      'arrivalDate',
+    );
+  });
+});
