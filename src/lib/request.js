1248e82302e5ccbce64abdd1960c357161ff3257
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-21 22:42:40 +0100

    GREEN: Implementing lib/request.js : buildQueryString()

--- src/lib/request.js
+++ src/lib/request.js
@@ -5,5 +5,7 @@ export function buildParam(key, value) {
 }
 
 export function buildQueryString(parameters) {
-
+  return Object.keys(parameters)
+    .map(key => buildParam(key, parameters[key]))
+    .join('&');
 }
