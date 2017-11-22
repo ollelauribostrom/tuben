c7fbc4f9995db39f4e2feb30572fcbc085198752
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-22 20:51:28 +0100

    GREEN: Implementing lib/dataParser.js : getLegType()

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export const transportationTypes = {
+  METRO: { name: 'METRO', symbol: '🚇', char: 'T' },
+  BUS: { name: 'BUS', symbol: '🚌', char: 'B' },
+  TRAIN: { name: 'TRAIN', symbol: '🚆', char: 'J' },
+  TRAM: { name: 'TRAM', symbol: '🚋', char: 'L' },
+  FERRY: { name: 'FERRY', symbol: '⛴', char: 'W' },
+  SHIP: { name: 'SHIP', symbol: '🚢', char: 'W' },
+  default: { name: 'OTHER', symbol: '🔘', char: '?' },
+};
--- src/lib/dataParser.js
+++ src/lib/dataParser.js
@@ -1,9 +1,11 @@
+import { transportationTypes } from '../config';
+
 export function getStation({ ResponseData: stations }) {
   const { Name: name, SiteId: id } = stations.shift();
   const similarStations = stations.map(station => station.Name);
   return { id, name, similarStations };
 }
 
-export function getLegType() {
-
+export function getLegType(typeName) {
+  return transportationTypes[typeName] || transportationTypes.default;
 }
