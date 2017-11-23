d60287bb343dc7d9e97e67904fc067bf5bb9b3a0
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-23 14:22:12 +0100

    REFACTOR: Refactoring lib/request.js : get()

--- src/lib/request.js
+++ src/lib/request.js
@@ -19,6 +19,6 @@ export function buildUrl(url, parameters) {
 }
 
 export async function get(url, parameters) {
-  const { data } = axios.get(buildUrl(url, parameters));
+  const { data } = await axios.get(buildUrl(url, parameters));
   return data;
 }
