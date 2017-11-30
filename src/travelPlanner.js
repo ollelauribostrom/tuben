6c21dc02222e27a53f79657bd7b23464593b858b
Olle Lauri Boström <ollebostr@gmail.com>
2017-11-30 15:27:00 +0100

    GREEN: Implementing travelPlanner.js : searchForStation()

--- src/travelPlanner.js
+++ src/travelPlanner.js
@@ -1,3 +1,9 @@
-export async function searchForStation() {
+import { getStation } from './lib/dataParser';
+import { get } from './lib/request';
+import { stationUrl, getStationKey } from './config';
 
+export async function searchForStation(searchstring) {
+  return getStation(
+    await get(stationUrl, { searchstring, key: getStationKey() }),
+  );
 }
