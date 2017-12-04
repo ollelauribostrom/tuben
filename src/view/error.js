4b6d2e97adec1f268e4051569b8f60f32e5f9543
Olle Lauri Boström <ollebostr@gmail.com>
2017-12-04 10:46:57 +0100

    REFACTOR: Rename cliview.js -> view/error.js & refactoring

---
+++ src/view/error.js
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+import { bugs } from '../../package.json';
+import { log } from './logger';
+
+export function generateErrorMessage(message = 'Please try again!') {
+  const header = `${chalk.white.bgRed('Sorry, something went wrong.')} 😢`;
+  const footer = `If the problem persists - open an issue @ ${chalk.blue(bugs.url)}`;
+  return `${header}\n${message}\n${footer}`;
+}
+
+export function printError(error) {
+  if (error.response) {
+    const serverError = error.response.status >= 500;
+    const serverErrorMessage = generateErrorMessage('We´re having some issues with our server / Trafiklabs API.');
+    const clientErrorMessage = generateErrorMessage();
+    return log(serverError ? serverErrorMessage : clientErrorMessage);
+  }
+
+  if (error.request) {
+    return log(generateErrorMessage('Please check your internet connection.'));
+  }
+
+  return log(generateErrorMessage());
+}
---
+++ test/error.test.js
@@ -0,0 +1,59 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import proxyquire from 'proxyquire';
+import * as logger from '../src/view/logger';
+
+const view = proxyquire('../src/view/error', {
+  chalk: {
+    white: {
+      bgRed: text => text,
+    },
+    blue: text => text,
+  },
+});
+
+function getResponseError(status = 500) {
+  const error = new Error();
+  error.response = { status };
+  return error;
+}
+
+describe('{unit}: view/error.js', () => {
+  let log;
+
+  before(() => {
+    log = sinon.stub(logger, 'log');
+  });
+
+  after(() => log.restore());
+
+  it('should log error message when error object contains response (500 range)', () => {
+    const expected = 'Sorry, something went wrong. 😢\nWe´re having some issues with our server / Trafiklabs API.\nIf the problem persists - open an issue @ https://github.com/ollelauribostrom/tuben/issues';
+    view.printError(getResponseError(500));
+    const actual = log.getCall(0).args[0];
+    return expect(actual).to.equal(expected);
+  });
+
+  it('should log error message when error object contains response (400 range)', () => {
+    const expected = 'Sorry, something went wrong. 😢\nPlease try again!\nIf the problem persists - open an issue @ https://github.com/ollelauribostrom/tuben/issues';
+    view.printError(getResponseError(400));
+    const actual = log.getCall(1).args[0];
+    return expect(actual).to.equal(expected);
+  });
+
+  it('should log error message when there was a problem with the request', () => {
+    const error = new Error();
+    error.request = {};
+    const expected = 'Sorry, something went wrong. 😢\nPlease check your internet connection.\nIf the problem persists - open an issue @ https://github.com/ollelauribostrom/tuben/issues';
+    view.printError(error);
+    const actual = log.getCall(2).args[0];
+    return expect(actual).to.equal(expected);
+  });
+
+  it('should log default message if error object does not contain response/request', () => {
+    const expected = 'Sorry, something went wrong. 😢\nPlease try again!\nIf the problem persists - open an issue @ https://github.com/ollelauribostrom/tuben/issues';
+    view.printError(new Error());
+    const actual = log.getCall(3).args[0];
+    return expect(actual).to.equal(expected);
+  });
+});
