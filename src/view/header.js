a49772f120bbee9408b54c3bf654c0dae201157e
Olle Lauri Boström <ollebostr@gmail.com>
2017-12-06 00:11:03 +0100

    REFACTOR: Refactoring view/header.js

--- src/view/header.js
+++ src/view/header.js
@@ -1,29 +1,22 @@
 import chalk from 'chalk';
-import cliWidth from 'cli-width';
 import { log } from './logger';
 
-export function padLines(text) {
-  return text
-    .split('\n')
-    .map(line => line.padEnd(cliWidth(), ' '))
-    .join('\n');
-}
+const description = `
 
-const description = padLines(`
  A CLI travel planner for SL (Storstockholms Lokaltrafik)
  Contribute @ https://github.com/ollelauribostrom/tuben
  -h, --help for help
- `);
+ `;
 
-const logo = padLines(`
+const logo = `
    _____           _                                 ___     _    
   |_   _|  _  _   | |__     ___    _ _       o O O  / __|   | |   
     | |   | +| |  | '_ \\   / -_)  | ' \\     o       \\__ \\   | |__ 
    _|_|_   \\_,_|  |_.__/   \\___|  |_||_|   TS__[O]  |___/   |____|
  _|"""""|_|"""""|_|"""""|_|"""""|_|"""""| {======|_|"""""|_|"""""|
- "\`-0-0-'"\`-0-0-'"\`-0-0-'"\`-0-0-'"\`-0-0-'./o--000'"\`-0-0-'"\`-0-0-'`);
+ "\`-0-0-'"\`-0-0-'"\`-0-0-'"\`-0-0-'"\`-0-0-'./o--000'"\`-0-0-'"\`-0-0-'`;
 
 export function printHeader() {
-  const header = `${chalk.white(logo)}${chalk.green(description)}`;
-  log(chalk.bgBlue(header));
+  const header = `${chalk.green(logo)}${chalk.blue(description)}`;
+  log(header);
 }
--- test/header.test.js
+++ test/header.test.js
@@ -5,11 +5,9 @@ import * as logger from '../src/view/logger';
 
 const header = proxyquire('../src/view/header', {
   chalk: {
-    white: text => text,
+    blue: text => text,
     green: text => text,
-    bgBlue: text => text,
   },
-  'cli-width': () => 100,
 });
 
 describe('{unit}: view/header.js', () => {
@@ -25,21 +23,6 @@ describe('{unit}: view/header.js', () => {
     it('should print header to console', () => {
       header.printHeader();
       return expect(log.calledOnce).to.be.true;
-    });
-  });
-
-  describe('padLines()', () => {
-    it('should return string padded at the end to with of cli', () => {
-      const actual = header.padLines('hej');
-      return expect(actual.length).to.equal(100);
-    });
-    it('should handle mutiple lines of text', () => {
-      const actual = header.padLines('hej\nhej');
-      return expect(actual.length).to.equal(201);
-    });
-    it('should pad with whitespace', () => {
-      const actual = header.padLines('hej');
-      return expect(actual.trim()).to.equal('hej');
     });
   });
 });
