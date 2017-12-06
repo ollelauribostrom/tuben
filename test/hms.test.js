cd50ca444441744f7e0993149ab9e6aa97e0211f
Olle Lauri Boström <ollebostr@gmail.com>
2017-12-06 21:27:25 +0100

    REFACTOR: Refactoring tests for lib/hms.js

--- test/hms.test.js
+++ test/hms.test.js
@@ -6,7 +6,7 @@ let getHours;
 let getMinutes;
 let getSeconds;
 
-function mockDatePrototype(hour, minute, second) {
+function mockDatePrototype({ hour, minute, second }) {
   getHours = sinon.stub(Date.prototype, 'getHours').returns(hour);
   getMinutes = sinon.stub(Date.prototype, 'getMinutes').returns(minute);
   getSeconds = sinon.stub(Date.prototype, 'getSeconds').returns(second);
@@ -18,52 +18,56 @@ function restoreDatePrototype() {
   getSeconds.restore();
 }
 
-describe('{unit}: lib/hms.js', () => {
+describe('{unit}: lib/hms.js getDifference()', () => {
   it('should return an object with calculated time difference { h, m, s }', () => {
+    const expected = { h: 0, m: 30, s: 0, difference: 1800000 };
     const actual = hms.getDifference('12:15:00', '12:45:00');
-    return expect(actual).to.deep.equal({ h: 0, m: 30, s: 0, difference: 1800000 });
+    return expect(actual).to.deep.equal(expected);
   });
 
   it('should handle 60 minutes as 1h, 0m', () => {
+    const expected = { h: 1, m: 0, s: 0, difference: 3600000 };
     const actual = hms.getDifference('12:00:00', '13:00:00');
-    return expect(actual).to.deep.equal({ h: 1, m: 0, s: 0, difference: 3600000 });
+    return expect(actual).to.deep.equal(expected);
   });
 
   it('should handle cases where startDate and endDate parameters are supplied to span over multiple dates', () => {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
+    const expected = { h: 0, m: 45, s: 0, difference: 2700000 };
     const actual = hms.getDifference('23:30:00', '00:15:00', new Date(), tomorrow);
-    return expect(actual).to.deep.equal({ h: 0, m: 45, s: 0, difference: 2700000 });
+    return expect(actual).to.deep.equal(expected);
   });
 
   it('should handle cases where only hour and minutes are provided', () => {
+    const expected = { h: 0, m: 2, s: 0, difference: 120000 };
     const actual = hms.getDifference('12:15', '12:17');
-    return expect(actual).to.deep.equal({ h: 0, m: 2, s: 0, difference: 120000 });
+    return expect(actual).to.deep.equal(expected);
   });
 
   it('should throw TypeError if two hms strings are not provided', () => {
-    const callToHmsDiffWithOneArgument = () => hms.getDifference('12:15:00');
-    return expect(callToHmsDiffWithOneArgument).to.throw(TypeError);
+    const callToGetDifference = () => hms.getDifference('12:15:00');
+    return expect(callToGetDifference).to.throw(TypeError);
   });
 
   it('should throw TypeError if hms string is in wrong format', () => {
-    const callToHmsDiffWithOneArgument = () => hms.getDifference('12.15', '12.19');
-    return expect(callToHmsDiffWithOneArgument).to.throw(TypeError);
+    const callToGetDifference = () => hms.getDifference('12.15', '12.19');
+    return expect(callToGetDifference).to.throw(TypeError);
   });
 
   it('should detect invalid hours (outside range 00-23) and throw TypeError', () => {
-    const callToHmsDiffWithOneArgument = () => hms.getDifference('12:15:00', '24:19:00');
-    return expect(callToHmsDiffWithOneArgument).to.throw(TypeError);
+    const callToGetDifference = () => hms.getDifference('12:15:00', '24:19:00');
+    return expect(callToGetDifference).to.throw(TypeError);
   });
 
   it('should detect invalid minutes (outside range 00-59) and throw TypeError', () => {
-    const callToHmsDiffWithOneArgument = () => hms.getDifference('12:15:00', '12:60:00');
-    return expect(callToHmsDiffWithOneArgument).to.throw(TypeError);
+    const callToGetDifference = () => hms.getDifference('12:15:00', '12:60:00');
+    return expect(callToGetDifference).to.throw(TypeError);
   });
 
   it('should detect invalid seconds (outside range 00-59) and throw TypeError', () => {
-    const callToHmsDiffWithOneArgument = () => hms.getDifference('12:15:60', '12:15:61');
-    return expect(callToHmsDiffWithOneArgument).to.throw(TypeError);
+    const callToGetDifference = () => hms.getDifference('12:15:60', '12:15:61');
+    return expect(callToGetDifference).to.throw(TypeError);
   });
 });
 
@@ -73,6 +77,7 @@ describe('{unit}: lib/hms.js validateDateObject()', () => {
     const callToValidateDateObject = () => hms.validateDateObject(new Date('invalid date'));
     return expect(callToValidateDateObject).to.throw(TypeError, expectedMessage);
   });
+
   it('should not throw TypError if called with valid Date Object', () => {
     const callToValidateDateObject = () => hms.validateDateObject(new Date());
     return expect(callToValidateDateObject).to.not.throw(TypeError);
@@ -130,24 +135,27 @@ describe('{unit}: lib/hms.js parseTimeString()', () => {
 
 describe('{unit}: lib/hms.js getTimeString()', () => {
   describe('...', () => {
-    before(() => mockDatePrototype(12, 10, 10));
+    before(() => mockDatePrototype({ hour: 12, minute: 10, second: 10 }));
     after(() => restoreDatePrototype());
 
     it('should return "now" as hh-mm-ss time string if called without any arguments', () => {
+      const expected = '12:10:10';
       const actual = hms.getTimeString();
-      return expect(actual).to.equal('12:10:10');
+      return expect(actual).to.equal(expected);
     });
 
     it('should excludeSeconds if option is set to true', () => {
+      const expected = '12:10';
       const actual = hms.getTimeString(new Date(), true);
-      return expect(actual).to.equal('12:10');
+      return expect(actual).to.equal(expected);
     });
 
     it('should pad single digits and always return format hh-mm-ss', () => {
       restoreDatePrototype();
-      mockDatePrototype(9, 9, 9);
+      mockDatePrototype({ hour: 9, minute: 9, second: 9 });
+      const expected = '09:09:09';
       const actual = hms.getTimeString();
-      return expect(actual).to.equal('09:09:09');
+      return expect(actual).to.equal(expected);
     });
   });
 
@@ -227,13 +235,15 @@ describe('{unit}: lib/hms.js isDateObject()', () => {
 
 describe('{unit}: lib/hms.js stripSeconds()', () => {
   it('should remove seconds from provided time string in format hh:mm:ss', () => {
+    const expected = '12:15';
     const actual = hms.stripSeconds('12:15:10');
-    return expect(actual).to.equal('12:15');
+    return expect(actual).to.equal(expected);
   });
 
   it('should return time string if seconds is not included', () => {
+    const expected = '12:15';
     const actual = hms.stripSeconds('12:15');
-    return expect(actual).to.equal('12:15');
+    return expect(actual).to.equal(expected);
   });
 
   it('should throw TypeError for invalid time strings', () => {
